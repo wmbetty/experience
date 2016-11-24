647e90e1b51d579092001b3bc53bdcb996c34733
Wmmrx <mei@gzruixun.com>
2016-11-24 11:32:00 +0800

    优支付改版

---
+++ VIP Center/js/forbid_fontsize.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Lenovo on 2016/11/24.
+ */
+//微信页面禁止字体放大
+(function(){
+    if (typeof(WeixinJSBridge) == "undefined") {
+        document.addEventListener("WeixinJSBridgeReady", function (e) {
+            setTimeout(function(){
+                WeixinJSBridge.invoke('setFontSizeCallback',{"fontSize":0}, function(res) {
+                    // alert(JSON.stringify(res));
+                });
+            },0);
+        });
+    } else {
+        setTimeout(function(){
+            WeixinJSBridge.invoke('setFontSizeCallback',{"fontSize":0}, function(res) {
+                // alert(JSON.stringify(res));
+            });
+        },0);
+    }
+})();
